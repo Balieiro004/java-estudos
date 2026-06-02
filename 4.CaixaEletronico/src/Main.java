da978dff3b3eb8bbc3a673b258e832636a51df1d
Balieiro004 <Balieiro004@gmail.com>
2026-06-02 20:38:16 -0300

    Aplicando condicionais no problema

--- 4.CaixaEletronico/src/Main.java
+++ 4.CaixaEletronico/src/Main.java
@@ -10,26 +10,41 @@ public class Main {
 
         int notas100 = saque / 100;
         int resto = saque % 100;
-        System.out.println(notas100 + " notas de 100");
+        if(notas100 > 0)
+            System.out.println(notas100 + " notas de 100");
 
         int notas50 = resto / 50;
         resto = resto % 50;
-        System.out.println(notas50 + " notas de 50");
+        if(notas50 == 1)
+            System.out.println(notas50 + " nota de 50");
+        else
+            System.out.println(notas50 + " notas de 50");
 
         int notas20 = resto / 20;
         resto = resto % 20;
-        System.out.println(notas20  + " notas de 20");
+        if(notas20 == 1)
+            System.out.println(notas20  + " nota de 20");
+        else
+            System.out.println(notas20  + " notas de 20");
 
         int notas10 = resto / 10;
         resto = resto % 10;
-        System.out.println(notas10  + " notas de 10");
+        if(notas10 == 1)
+            System.out.println(notas10  + " nota de 10");
+        else
+            System.out.println(notas10  + " notas de 10");
 
         int notas5 = resto / 5;
         resto = resto % 5;
-        System.out.println(notas5  + " notas de 5");
+        if(notas5 == 1)
+            System.out.println(notas5  + " nota de 5");
+        else
+            System.out.println(notas5  + " notas de 5");
 
         int notas1 = resto;
-        resto = resto % 1;
-        System.out.println(notas1  + " notas de 1");
+        if(notas1 == 1)
+            System.out.println(notas1  + " nota de 1");
+        else
+            System.out.println(notas1  + " notas de 1");
     }
 }
