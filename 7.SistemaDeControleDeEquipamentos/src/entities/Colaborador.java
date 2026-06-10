5c89a21ccbfd86ca899a62da0a14e87727bb85ae
Balieiro004 <Balieiro004@gmail.com>
2026-06-09 21:59:03 -0300

    Criando estrutura do projeto

---
+++ 7.SistemaDeControleDeEquipamentos/src/entities/Colaborador.java
@@ -0,0 +1,48 @@
+package entities;
+
+public class Colaborador {
+
+    private static int contador;
+    private int id;
+    private String nome;
+    private String email;
+
+
+    public Colaborador(int id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Colaborador" +
+                "\nid:" + id +
+                "\nnome: " + nome +
+                "\nemail: " + email ;
+    }
+}
