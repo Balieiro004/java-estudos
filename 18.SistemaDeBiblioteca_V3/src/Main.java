1882a6ab344000b87e63f96f1e11ba6bf3341812
balieiro004 <balieiro004@gmail.com>
2026-09-03 19:24:33 -0300

    feat: cria entidade livro e regras de status

--- 18.SistemaDeBiblioteca_V3/src/Main.java
+++ 18.SistemaDeBiblioteca_V3/src/Main.java
@@ -1,5 +1,25 @@
+import entities.Livro;
+
 public class Main {
     public static void main(String[] args) {
+
+
+        Livro livro = new Livro(
+                "O Senhor dos Anéis",
+                "J.R.R. Tolkien",
+                "978-0000000000",
+                1954
+        );
+
+        System.out.println(livro);
+
+        livro.emprestado();
+
+        System.out.println("=========");
+        System.out.println(livro);
+
+        livro.disponivel();
+        livro.disponivel();
 
     }
 }
--- 18.SistemaDeBiblioteca_V3/src/entities/Livro.java
+++ 18.SistemaDeBiblioteca_V3/src/entities/Livro.java
@@ -12,13 +12,87 @@ public class Livro {
     private int anoPublicacao;
     private StatusLivro statusLivro;
 
-    public Livro(String titulo, String autor, String isbn, int anoPublicacao, StatusLivro statusLivro) {
+    public Livro(String titulo, String autor, String isbn, int anoPublicacao) {
         contador++;
         this.id = contador;
         this.titulo = titulo;
         this.autor = autor;
         this.isbn = isbn;
         this.anoPublicacao = anoPublicacao;
-        this.statusLivro = statusLivro;
+        this.statusLivro = StatusLivro.DISPONIVEL;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public StatusLivro getStatusLivro() {
+        return statusLivro;
+    }
+
+    public void disponivel(){
+        validarSeLivroEstaEmprestado();
+        this.statusLivro = StatusLivro.DISPONIVEL;
+    }
+
+    public void emprestado(){
+        validarSeLivroEstaDisponivel();
+        this.statusLivro = StatusLivro.EMPRESTADO;
+    }
+
+    private void validarSeLivroEstaDisponivel(){
+
+        if (statusLivro != StatusLivro.DISPONIVEL) {
+            throw new IllegalStateException("O livro não está disponível para empréstimo.");
+        }
+    }
+
+    private void validarSeLivroEstaEmprestado() {
+        if (statusLivro != StatusLivro.EMPRESTADO) {
+            throw new IllegalStateException("O livro não está emprestado.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "========Livro========" +
+                "\nId: " + id +
+                "\nTitulo: " + titulo  +
+                "\nAutor: " + autor +
+                "\nIsbn: " + isbn +
+                "\nAno Publicacao: " + anoPublicacao +
+                "\nStatus Livro: " + statusLivro;
     }
 }
