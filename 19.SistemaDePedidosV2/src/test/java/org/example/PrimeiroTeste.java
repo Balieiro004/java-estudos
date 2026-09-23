50d774b4fbfded550348f4ea650566800eafc601
balieiro004 <balieiro004@gmail.com>
2026-09-23 10:55:52 -0300

    Criando o projeto e configurando Maven com JUnit

---
+++ 19.SistemaDePedidosV2/src/test/java/org/example/PrimeiroTeste.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class PrimeiroTeste {
+
+    @Test
+    void primeiroTest() {
+        System.out.println("JUnit está funcionando!");
+    }
+}
